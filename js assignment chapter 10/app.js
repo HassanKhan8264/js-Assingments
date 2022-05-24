2cfd452e88171786a21da5537c11fdf575a323fc
Hassan Khan <hf610061@gmail.com>
2022-05-23 17:08:56 -0700

    new

--- js assignment chapter 10/app.js
+++ js assignment chapter 10/app.js
@@ -11,16 +11,178 @@ var ten = 10;
 var gessNumber = prompt("Enter any secret number from 1 to 10");
 if (gessNumber = one) {
     alert("Bingo your are correct");
-}
-var guessNumber = prompt("Enter any secret number from 1 to 10");
-if (guessNumber = two) {
+} else (gessNumber != one);{
     alert("Your are close to that");
 }
+var divisible = prompt("Enter the even number");
+if(divisible = 9 ){
+    alert("The number you have dail is divisible by 3");
 
-// if(gessNumber = seven){
-//     alert("Bingoo Your are correct");
-    
-//   if(gessNumber = eight){
-//         alert("you are near");
-//     }
-// }
+}
+var quizTeamA = prompt("Enter older name of karachi");
+var score = 1;
+if(quizTeamA = "kolachi"){
+    alert("you are correct");
+    alert("score =" + score);
+
+}
+var quizTeamA = prompt("Enter older name of karachi");
+var score = 1;
+if(quizTeamA != "kolachi"){
+    alert("you are wrong");
+    alert("score =" + --score);
+
+}
+var quizTeamA = prompt("Enter html stands for");
+var score = 1;
+if(quizTeamA = "hypertextmarkuplangualge"){
+    alert("you are correct");
+    alert("score =" + ++score);
+}
+var quizTeamA = prompt("Enter html stands for");
+var score = 1;
+if(quizTeamA != "hypertextmarkuplangualge"){
+    alert("you are wrong");
+    alert("score =" + --score);
+}
+var quizTeamB = prompt("What is capital of Pakistan");
+if(quizTeamB = "Islamabad"){
+    alert("You are correct");
+    alert("score =" + score);
+}
+var quizTeamB = prompt("What is capital of Pakistan");
+if(quizTeamB != "Islamabad"){
+    alert("You are wrong");
+    alert("score =" + --score);
+}
+var quizTeamB = prompt("do you  want to travel the world and never come back");
+if(quizTeamB = "Yes"){
+    alert("You are correct");
+    alert("score =" + ++score);
+}
+var quizTeamB = prompt("do you  want to travel the world and never come back");
+if(quizTeamB = "no"){
+    alert("i dont like you");
+    alert("score =" + --score);
+}
+var string = "Pakistan";
+var number = 3;
+var boolean = "true is boolean";
+var questionS = prompt("is this is string ?" + string);
+if(questionS = "yes"){
+    alert("Pakistan is the string");
+}
+var questionS = prompt("is this is number ?" + number );
+if(questionS = "yes"){
+    alert("3 is a nnumber");
+}
+var questionS = prompt("is this is boolean ?");
+if(questionS = "yes"){
+    alert("true is a boolean");
+}
+var evenNumber = prompt("Is this is the even number or odd");
+if(evenNumber = "2"){
+    alert("this is the even number");
+}
+var oddNumber = prompt("Is this is the even number or odd");
+if(evenNumber = "3"){
+    alert("this is the odd number");
+}
+var t = prompt("What is the temp today in karachi");
+if(t <= 40){
+    alert("its too hot outside");
+
+}
+var t = prompt("What is the temp today in karachi");
+if(t >= "30"){
+    alert("the weather is normal today");
+
+}
+var t = prompt("What is the temp today in karachi");
+if(t >= "20"){
+    alert("Today's Weather is cool");
+}
+var t = prompt("What is the temp today in karachi");
+if(t >= "10"){
+    alert("OMG Today's weather is so cool");
+}
+var numberOne = 2;
+var numberTwo = 2;
+var perform = prompt("perform any mathemetical operation");
+if(perform = "4"){
+    alert("its 4");
+}
+var perform = prompt("perform any mathemetical operation");
+if(perform = "4"){
+    alert("Its 4");
+}
+var perform = prompt("perform any mathemetical operation");
+if(perform = "0"){
+    alert("its 0");
+}
+var perform = prompt("perform any mathemetical operation");
+if(perform = "0"){
+    alert("its 0");
+}
+var days = prompt("Enter days");
+if(days = "monday"){
+    alert("its week day")
+
+}
+if(days = "tuesday"){
+    alert("its week day")
+
+}
+if(days = "wednesday"){
+    alert("its week day")
+
+}
+if(days = "thursday"){
+    alert("its week day")
+
+}
+if(days = "friday"){
+    alert("its week day")
+
+}
+if(days = "saturday"){
+    alert("Its weekend");
+
+}
+if(days = "sunday"){
+    alert("Yay!", "its a holiday");
+
+}
+var score = prompt("Enter your score")
+if(score = "50"){
+    alert("You are passed the test");
+}
+var numbers = prompt("Enter two number i'll tell you is equla or not") 
+if(numbers = "2 = 2"){
+    alert("This number is equal");
+}
+if(numbers <= "2  4"){
+    alert("This 2 is less than 4");
+}
+var helloWorld = prompt("Enter the language");
+var eng = "Hello Wolrd!";
+var frnch = "Bonjour le monde";
+var span = "Hola Mundo";
+if(helloWorld = "english"){
+    alert("Hello Wrold");
+}
+if(helloWorld = "spanish"){
+    alert("Hola Mundo");
+}
+if(helloWorld = "french"){
+    alert("Bonjo le monde");
+}
+var positiveNumber = prompt("Enter a number");
+if(positiveNumber = "2"){
+    alert("Its positive number");
+}
+if(positiveNumber = "-2"){
+    alert("Its nagative number");
+}
+var noun = prompt("Enter a noun");
+var number = prompt("Enter a number");
